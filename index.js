c52f35d4f548ec89bdd79aa05806ff6591e49546
Ridwan346 <ridwanhossain955@gmail.com>
2026-04-19 21:37:01 +0600

    prosecc

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
